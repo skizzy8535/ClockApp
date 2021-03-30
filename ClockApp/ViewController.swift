12428b2c02c67d4b145238cddda0d814ebe57ad5
Yuchen <smirkiiiv@gmail.com>
2021-03-30 10:53:27 +0800

    Initial Commit

---
+++ ClockApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ClockApp
+//
+//  Created by 林祐辰 on 2021/3/30.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
